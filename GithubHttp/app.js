b75ccd1a3e98d20c95d1b4e83c5006f8933411ba
churtado <charliepapa2@gmail.com>
2014-08-02 15:15:15 +0200

    improvements

--- GithubHttp/app.js
+++ GithubHttp/app.js
@@ -6,31 +6,27 @@ var MainController = (function () {
     function MainController($scope, $http) {
         var onUserComplete = function (response) {
             $scope.user = response.data;
+            $http.get($scope.user.repos_url).then(onRepos, onError);
         };
 
         var onError = function (reason) {
             $scope.error = "Could not fetch the user";
         };
 
-        $http.get("https://api.github.com/users/robconery").then(onUserComplete, onError);
+        var onRepos = function (response) {
+            $scope.repos = response.data;
+        };
 
+        $scope.search = function (username) {
+            $http.get("https://api.github.com/users/" + username).then(onUserComplete, onError);
+        };
+
+        $scope.username = "angular";
         $scope.message = "Github example using $http and promises";
+        $scope.repoSortOrder = "-stargazers_count";
     }
     return MainController;
 })();
 
 githubModule.controller("mainController", MainController);
-/*
-var MainController = function ($scope, $http) {
-var onUserComplete = function (response) {
-$scope.user = response.data;
-}
-var onError = function (reason) {
-$scope.error = "Could not fetch the user";
-};
-$http.get("https://api.github.com/users/robconery")
-.then(onUserComplete, onError);
-$scope.message = "Github example using $http and promises";
-};
-*/
 //# sourceMappingURL=app.js.map
--- GithubHttp/app.ts
+++ GithubHttp/app.ts
@@ -1,9 +1,13 @@
 ﻿/// <reference path="Scripts/typings/angularjs/angular.d.ts" />
 
 interface gitHubScope extends ng.IScope {
-    user: Object;
+    user: any;
     error: string;
     message: string;
+    username: string;
+    repos: any;
+    search(username: string);
+    repoSortOrder: string;
 }
 
 var githubModule = angular.module("githubModule", []);
@@ -14,37 +18,28 @@ class MainController {
 
         var onUserComplete = function (response) {
             $scope.user = response.data;
+            $http.get($scope.user.repos_url)
+                .then(onRepos, onError);
         }
 
         var onError = function (reason) {
             $scope.error = "Could not fetch the user";
         };
 
-        $http.get("https://api.github.com/users/robconery")
-            .then(onUserComplete, onError);
+        var onRepos = function (response) {
+            $scope.repos = response.data;
+        }
 
+        $scope.search = function (username) {
+            $http.get("https://api.github.com/users/" + username)
+                .then(onUserComplete, onError);
+        }
+
+        $scope.username = "angular";
         $scope.message = "Github example using $http and promises";
-
+        $scope.repoSortOrder = "-stargazers_count";
     }
 
 }
 
 githubModule.controller("mainController", MainController);
-
-/*
-var MainController = function ($scope, $http) {
-
-    var onUserComplete = function (response) {
-        $scope.user = response.data;
-    }
-
-    var onError = function (reason) {
-        $scope.error = "Could not fetch the user";
-    };
-
-    $http.get("https://api.github.com/users/robconery")
-        .then(onUserComplete, onError);
-
-    $scope.message = "Github example using $http and promises";
-};
-*/
