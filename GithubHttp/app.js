91ebe63e560a128b1d2922d98f2ed0715ac930ea
churtado <charliepapa2@gmail.com>
2014-08-02 13:44:27 +0200

    first commit

---
+++ GithubHttp/app.js
@@ -0,0 +1,36 @@
+﻿/// <reference path="Scripts/typings/angularjs/angular.d.ts" />
+
+var githubModule = angular.module("githubModule", []);
+
+var MainController = (function () {
+    function MainController($scope, $http) {
+        var onUserComplete = function (response) {
+            $scope.user = response.data;
+        };
+
+        var onError = function (reason) {
+            $scope.error = "Could not fetch the user";
+        };
+
+        $http.get("https://api.github.com/users/robconery").then(onUserComplete, onError);
+
+        $scope.message = "Github example using $http and promises";
+    }
+    return MainController;
+})();
+
+githubModule.controller("mainController", MainController);
+/*
+var MainController = function ($scope, $http) {
+var onUserComplete = function (response) {
+$scope.user = response.data;
+}
+var onError = function (reason) {
+$scope.error = "Could not fetch the user";
+};
+$http.get("https://api.github.com/users/robconery")
+.then(onUserComplete, onError);
+$scope.message = "Github example using $http and promises";
+};
+*/
+//# sourceMappingURL=app.js.map
---
+++ GithubHttp/app.ts
@@ -0,0 +1,50 @@
+﻿/// <reference path="Scripts/typings/angularjs/angular.d.ts" />
+
+interface gitHubScope extends ng.IScope {
+    user: Object;
+    error: string;
+    message: string;
+}
+
+var githubModule = angular.module("githubModule", []);
+
+class MainController {
+
+    constructor($scope:gitHubScope, $http:ng.IHttpService) {
+
+        var onUserComplete = function (response) {
+            $scope.user = response.data;
+        }
+
+        var onError = function (reason) {
+            $scope.error = "Could not fetch the user";
+        };
+
+        $http.get("https://api.github.com/users/robconery")
+            .then(onUserComplete, onError);
+
+        $scope.message = "Github example using $http and promises";
+
+    }
+
+}
+
+githubModule.controller("mainController", MainController);
+
+/*
+var MainController = function ($scope, $http) {
+
+    var onUserComplete = function (response) {
+        $scope.user = response.data;
+    }
+
+    var onError = function (reason) {
+        $scope.error = "Could not fetch the user";
+    };
+
+    $http.get("https://api.github.com/users/robconery")
+        .then(onUserComplete, onError);
+
+    $scope.message = "Github example using $http and promises";
+};
+*/
